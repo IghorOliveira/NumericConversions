ab146288d41d9c25a678bda85140e0ce7609e393
Ighor Alves de Oliveira <75804433+IghorOliveira@users.noreply.github.com>
2021-01-14 12:22:20 -0300

    Update IntegerToRoman.java

--- RomanNumberConverter/IntegerToRoman.java
+++ RomanNumberConverter/IntegerToRoman.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package RomanNumberConverter;
 
 import java.util.Scanner;
@@ -33,9 +28,5 @@ public class IntegerToRoman {
         }
         
         System.out.println("Result: "+result);
-        
-
-        
-        
     }
 }
