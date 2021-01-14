45252cbb4a237f8472b8b1039452f4750cdb465e
Ighor Alves de Oliveira <75804433+IghorOliveira@users.noreply.github.com>
2021-01-14 12:19:55 -0300

    Update DecimalToBinary.java

--- BinaryConverter/DecimalToBinary.java
+++ BinaryConverter/DecimalToBinary.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package BinaryConverter;
 
 import java.util.ArrayList;
@@ -14,7 +9,6 @@ import java.util.Scanner;
  */
 public class DecimalToBinary {
     public static void main(String args[]){
-        
         //Setting things up
         Scanner myObj = new Scanner(System.in);  
         System.out.println("Enter the integer number");
@@ -28,6 +22,5 @@ public class DecimalToBinary {
         
         result = userInput + result;
         System.out.println("Result: "+result);
-
     }
 }
