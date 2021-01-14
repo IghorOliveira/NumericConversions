37bd1e2334553a27b599d1d9a2c4d5cb246c7c19
Ighor Alves de Oliveira <75804433+IghorOliveira@users.noreply.github.com>
2021-01-14 12:26:19 -0300

    Update RomanToInteger.java

--- RomanNumberConverter/RomanToInteger.java
+++ RomanNumberConverter/RomanToInteger.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package RomanNumberConverter;
 
 import java.util.Scanner;
@@ -14,7 +9,6 @@ import java.util.Scanner;
 public class RomanToInteger {
     
     //Max conversion: MMMCMXCIX -> 3999
-    
     public static void main(String args[]){
         
         //Setting things up
@@ -22,61 +16,55 @@ public class RomanToInteger {
             //And getting user input
         System.out.println("Enter the roman number");
         String userInput = myObj.nextLine().toUpperCase();
-        String[] numerais = new String[userInput.length()];
-        int[] numeros = new int[userInput.length()];
+        String[] numerals = new String[userInput.length()];
+        int[] numbers = new int[userInput.length()];
         int result=0;
         
-        
         for(int i = 0; i <= userInput.length()-1; i++){
-            numerais[i]=userInput.charAt(i)+"";
+            numerals[i]=userInput.charAt(i)+"";
             
-            if(numerais[i].equals("I")){
-                numeros[i] = 1;
+            if(numerals[i].equals("I")){
+                numbers[i] = 1;
             } 
-            if(numerais[i].equals("V")){
-                numeros[i] = 5;    
+            if(numerals[i].equals("V")){
+                numbers[i] = 5;    
             } 
-            if(numerais[i].equals("X")){
-                numeros[i] = 10;    
+            if(numerals[i].equals("X")){
+                numbers[i] = 10;    
             } 
-            if(numerais[i].equals("L")){
-                numeros[i] = 50;    
+            if(numerals[i].equals("L")){
+                numbers[i] = 50;    
             } 
-            if(numerais[i].equals("C")){
-                numeros[i] = 100;    
+            if(numerals[i].equals("C")){
+                numbers[i] = 100;    
             } 
-            if(numerais[i].equals("D")){
-                numeros[i] = 500;    
+            if(numerals[i].equals("D")){
+                numbers[i] = 500;    
             } 
-            if(numerais[i].equals("M")){
-                numeros[i] = 1000;    
+            if(numerals[i].equals("M")){
+                numbers[i] = 1000;    
             } 
-            
             
             if(i>=1){
-                //Se o numero anterior for menor q o atual
-                if(numeros[i]>numeros[i-1]){
-                    //Subtrai
-                    result += numeros[i]-(2*numeros[i-1]);
+                //If the prev number is smaller then the current one
+                if(numbers[i]>numbers[i-1]){
+                    //Subtract it
+                    result += numbers[i]-(2*numbers[i-1]);
                 } else{
-                    //Soma
-                    result += numeros[i];
+                    //Add it
+                    result += numbers[i];
                 }
-                
             } else if(i==0){
-                //String the variable
-                result=numeros[i];
+                //Adding the furst number to tha var
+                result=numbers[i];
             }
             
-            System.out.println("The Roman Numeral "+numerais[i]+" is equal to "+numeros[i]);
+            System.out.println("The Roman Numeral "+numerals[i]+" is equal to "+numbers[i]);
         }
         if(result<0){
             result=result *(-1);
         }
-        
+  
         System.out.println(userInput+" converts to "+result);
     }
-    
-    
-    
 }
