115dec3c5b22cc4f0b4c3872fe6a6a0487bb9ccf
Ighor Alves de Oliveira <75804433+IghorOliveira@users.noreply.github.com>
2021-01-14 12:20:24 -0300

    Update DecimalToHexa.java

--- HexadecimalConverter/DecimalToHexa.java
+++ HexadecimalConverter/DecimalToHexa.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package HexadecimalConverter;
 
 import java.util.Scanner;
@@ -13,7 +8,6 @@ import java.util.Scanner;
  */
 public class DecimalToHexa {
     public static void main(String args[]){
-        
         //Setting things up
         Scanner myObj = new Scanner(System.in);  
         System.out.println("Enter the integer number");
